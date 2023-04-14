ddb2366135a3b670461dcd9da79c175e33402779
cpding2000 <95485588+cpding2000@users.noreply.github.com>
2023-04-14 12:24:30 -0400

    Update twister_game.py

--- twister_game.py
+++ twister_game.py
@@ -6,8 +6,6 @@ class Player:
     
     Attributes:
         name(str): The name of the player
-        spinner_colors(list of strings):A list containing all the four possible colors(red,blue,green,yellow,spinner_choice) a player can move to.
-        spinner_body_parts(list of strings):A list containing the four possible body parts(left/right hand,left/right foot,sinner_choice) that a player can place on the board
         right_foot(int): A number(1) that represents the right foot
         left_foot(int): A number(2) that represents the left foot
         right_hand(int): A number(3) that represents the right hand
@@ -22,7 +20,7 @@ class Player:
             name(str): the name of the player
             
         Side effects:
-            Initializes attribute name,spinner_colors,spinner_body_parts,right_foot,left_foot,right_hand,left_hand
+            Initializes attribute name,right_foot,left_foot,right_hand,left_hand
         """
      def turn(self,name):
         """Executes a player turn. Uses sequence unpacking.
@@ -34,12 +32,36 @@ class Player:
         Side effects:
             Prompts the current player to spin for a body part and a color.
         """
+    
+        
+
+class Board:
+    """The board the players use for the game Twister.
+    
+    Attributes:
+        spinner_colors(list of strings):A list containing all the four possible colors(red,blue,green,yellow,spinner_choice) a player can move to.
+        spinner_body_parts(list of strings):A list containing the four possible body parts(left/right hand,left/right foot,spinner_choice) that a player can place on the board
+        board (dictionary): Dictionary that represents each circle on the board and whether or not it is occupied.
+        player (Player object): A player participating in the game.
+        
+    """
+    
+    def __init__(self, board):
+        """Initializes a Board object.
+        
+        Args:
+            board (dictionary): Dictionary that represents each circle on the board and whether or not it is occupied.
+            
+        Side effects:
+             Initializes attribute board,spinner_colors,spinner_body_parts
+        """
      def spinner(self,spinner_colors,spinner_body_parts):
         """The spinner randomly selects a body part that the player will move and a color that the player will land on. We will be using a f-string in this method
         
         Args:
             spinner_colors(list of strings):A list containing all the four possible colors(red,blue,green,yellow,spinner_choice) a player can move to.
             spinner_body_parts(list of strings):A list containing the four possible body parts(left/right hand,left/right foot,sinner_choice) that a player can place on the board
+            
         Side effects:
             print the value that the spinner gives out. Alters the spinner list throught the use of append.
             """
@@ -53,28 +75,6 @@ class Player:
             Prints "Player lost and Other_Player won."
         """
         
-
-class Board:
-    """The board the players use for the game Twister.
-    
-    Attributes:
-        board (dictionary): Dictionary that represents each circle on the board and whether or not it is occupied.
-        player (Player object): A player participating in the game.
-        
-    """
-    
-    def __init__(self, board):
-        """Initializes a Board object.
-        
-        Args:
-            board (dictionary): Dictionary that represents each circle on the board and whether or not it is occupied.
-        """
-    
-    
-        
-    def __str__(self):
-        """"Returns a representation of the board. We will be using a magic method in this method.""""
-        
     def board_adjustment(self, player):
         """Keeps track of where the players are on the board. Uses dictionary comprehension.
         
@@ -86,6 +86,10 @@ class Board:
         
         
         """
+    
+        
+    def __str__(self):
+        """"Returns a representation of the board. We will be using a magic method in this method.""""
         
 
 
